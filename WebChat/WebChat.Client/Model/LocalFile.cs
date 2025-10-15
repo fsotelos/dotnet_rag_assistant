8b5ff64bf2b7164f9c1fc621a8ea67a1eb6f6a2e
Felipe Armando Sotelo Sotelo <felipe.sotelo@live.com>
2025-10-15 13:31:44 -0500

    remove first version

--- WebChat/WebChat.Client/Model/LocalFile.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebChat.Client.Model
-{
-    public class LocalFile
-    {
-        public string FileName { get; set; } = string.Empty;
-        public byte[] Content { get; set; } = [];
-    }
-}
